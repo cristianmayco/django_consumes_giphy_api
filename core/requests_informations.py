389bb63fb0e7a468a3f151e85df863ffed3a54d7
Cristian Mayco da Costa <cristianmayco@gmail.com>
2020-08-28 12:23:48 -0300

    Added icon url

--- core/requests_informations.py
+++ core/requests_informations.py
@@ -8,5 +8,6 @@ class RequestsConfigurations(Enum):
     API_KEY = 'zPfJHNJ2bGCpUJr7jyvcW0RcctC2QOUN'
     LIMIT = 10
     RATING = 'g'
+    ICON = 'https://developers.giphy.com/static/img/dev-logo-lg.7404c00322a8.gif'
 
 
